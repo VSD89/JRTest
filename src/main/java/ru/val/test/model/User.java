c779eb4c9488cd5c776d67f9ae5821d62b61eea4
VSD89 <vr_89@mail.ru>
2016-11-24 22:29:10 +0700

    Initial commit

---
+++ src/main/java/ru/val/test/model/User.java
@@ -0,0 +1,77 @@
+package ru.val.test.model;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "AGE")
+    private int age;
+
+    @Column(name = "IS_ADMIN")
+    private boolean Admin;
+
+    @Column(name = "CREATED_DATE")
+    private Date date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isAdmin() {
+        return Admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        Admin = admin;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", Admin=" + Admin +
+                ", date=" + date +
+                '}';
+    }
+}
